5391cf1e3af2edab3a02fb4991a99932e8ddf82c
saeedkaz <saeed.kazemy@gmail.com>
2023-01-20 01:12:03 +0330

    decoarte output

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	fmt.Println("---Namespaces---")
 	NamespaceList, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 	for _, n := range NamespaceList.Items {
-		fmt.Println("--- namespace", n.Name, "---")
+		fmt.Println("namespace:", n.Name)
 
 		pods, err := clientset.CoreV1().Pods(n.Name).List(ctx, metav1.ListOptions{})
 		fmt.Println("---Pods from namespace:", n.Name, "---")
