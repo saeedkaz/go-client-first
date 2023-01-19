f0f411e054ed4858991fa996069c52519133d15e
saeedkaz <saeed.kazemy@gmail.com>
2023-01-20 01:24:24 +0330

    limit printed data

--- main.go
+++ main.go
@@ -43,17 +43,17 @@ func main() {
 	fmt.Println("---Namespaces---")
 	NamespaceList, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 	for _, n := range NamespaceList.Items {
-		fmt.Println("namespace:", n.Name)
+		//fmt.Println("namespace:", n.Name)
 
 		pods, err := clientset.CoreV1().Pods(n.Name).List(ctx, metav1.ListOptions{})
-		fmt.Println("---Pods from namespace:", n.Name, "---")
+		//fmt.Println("---Pods from namespace:", n.Name, "---")
 		if err != nil {
 			fmt.Println("error:pod loop in namespaces")
 
 		}
 		podcount := 0
 		for _, pod := range pods.Items {
-			fmt.Println(pod.Name)
+			//	fmt.Println(pod.Name)
 			podcount++
 		}
 		if podcount == 0 {
